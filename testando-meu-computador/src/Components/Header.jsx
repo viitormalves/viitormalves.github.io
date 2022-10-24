0926476b9114b0d367e12d3ec416c9abc74d85c1
Vitor M. Alves <vtr.vma@hotmail.com>
2022-10-24 10:58:44 -0300

    Reajustando cv

--- testando-meu-computador/src/Components/Header.jsx
+++ testando-meu-computador/src/Components/Header.jsx
@@ -11,6 +11,8 @@ class Header extends React.Component {
                 <div className="header-container-2">
                   <h3 className="h">Olá! Eu sou o Vítor Magalhães Alves</h3>
                   <h3 className="h">Estudante de Desenvolvedor Web Full-Stack</h3>
+                  <h4>vtr.vma@hotmail.com</h4>
+                  <a href="https://www.linkedin.com/in/viitormalves/" target="_blank" rel="noopener noreferrer">LindedIn</a>
                 </div>
             </header>
         )
